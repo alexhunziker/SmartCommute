e6de9e2ea888a8e57478d44dab51f27dee5e219f
caixinyuan <913052176@qq.com>
2018-07-05 22:17:19 +0200

    frontend UI -fix again infobox/button to classics/smart

--- app/src/main/java/com/example/compuhypermeganet/smart_commute/adapter/BikeTripAdapter.java
+++ app/src/main/java/com/example/compuhypermeganet/smart_commute/adapter/BikeTripAdapter.java
@@ -1,9 +1,6 @@
 package com.example.compuhypermeganet.smart_commute.adapter;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.compuhypermeganet.smart_commute.R;
 import com.example.compuhypermeganet.smart_commute.model.BikeTrip;
@@ -41,7 +37,7 @@ public class BikeTripAdapter extends BaseAdapter {
         if(trip.getBikeTrip()==null) return 0;
         for(int i =0;i<legList.size();i++){
             if(legList.get(i).getFrom().getName().equals(trip.getBikeTrip().getTransferStation().getName())){
-                return i+2;
+                return i+1;
             }
         }
         return legList == null ? 0:legList.size();}
@@ -59,51 +55,49 @@ public class BikeTripAdapter extends BaseAdapter {
         TextView arriv_time;
         TextView arriv_address;
     }
-    @SuppressLint("SetTextI18n")
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder vh = null;
+        if(convertView == null) {
+            convertView = inflater.inflate(resource,null);
+            vh = new BikeTripAdapter.ViewHolder();
+            vh.arriv_address =(TextView) convertView.findViewById(R.id.arriv_address);
+            vh.arriv_time = (TextView) convertView.findViewById(R.id.arriv_time);
+            vh.depart_time =(TextView) convertView.findViewById(R.id.depart_time);
+            vh.from = (TextView) convertView.findViewById(R.id.from);
+            vh.mode = (TextView) convertView.findViewById(R.id.mode);
 
-            if (convertView == null) {
-                convertView = inflater.inflate(resource, null);
-                vh = new BikeTripAdapter.ViewHolder();
-                vh.arriv_address = (TextView) convertView.findViewById(R.id.arriv_address);
-                vh.arriv_time = (TextView) convertView.findViewById(R.id.arriv_time);
-                vh.depart_time = (TextView) convertView.findViewById(R.id.depart_time);
-                vh.from = (TextView) convertView.findViewById(R.id.from);
-                vh.mode = (TextView) convertView.findViewById(R.id.mode);
+            convertView.setTag(vh);
+        } else {
+            vh = (ViewHolder) convertView.getTag();
 
-                convertView.setTag(vh);
-            } else {
-                vh = (ViewHolder) convertView.getTag();
-
-            }
-            Leg leg = getItem(position);
-            if (leg != null) {
-                if (trip.getBikeTrip() == null || !leg.getFrom().getName().equals(trip.getBikeTrip().getTransferStation().getName())) {
-                    //normal plan
-                    vh.depart_time.setText(leg.getDeparture().toString().substring(11, 16));
-                    vh.arriv_time.setText(leg.getArrival().toString().substring(11, 16));
-                    vh.arriv_address.setText(leg.getTo().getName());
-                    vh.from.setText(leg.getFrom().getName());
-                    if (leg.getLine() != null) {
-                        vh.mode.setText(leg.getLine() + " " + leg.getDirection());
-                        Log.d("line", leg.getLine() + " " + leg.getDirection());
-                    }
-                    Log.d("visibility", "show");
-
-                } else {
-                    BikeTrip bt = trip.getBikeTrip();
-                    vh.depart_time.setText(bt.getDepartureTime().toString().substring(11, 16));
-                    vh.arriv_time.setText(bt.getArrivalTime().toString().substring(11, 16));
-                    vh.arriv_address.setText(bt.getTo().getAddress());
-                    vh.from.setText(bt.getTransferStation().getName());
-                    vh.mode.setText("By Callabike " + bt.getAvailability() + " bikes available at " + bt.getTransferStation().getName() + " Duration " + (int) bt.getDuration() + " min Distance " + (int) bt.getDistance() + " m");
-                    Log.d("visibility", "gone");
+        }
+        Leg leg = getItem(position);
+        if(leg!=null) {
+            if(trip.getBikeTrip() == null || !leg.getFrom().getName().equals(trip.getBikeTrip().getTransferStation().getName())){
+                //normal plan
+                vh.depart_time.setText(leg.getDeparture().toString().substring(11, 16));
+                vh.arriv_time.setText(leg.getArrival().toString().substring(11, 16));
+                vh.arriv_address.setText(leg.getTo().getName());
+                vh.from.setText(leg.getFrom().getName());
+                if(leg.getLine()!=null){
+                    vh.mode.setText(leg.getLine() + " " + leg.getDirection());
+                    Log.d("line",leg.getLine() + " " + leg.getDirection());
                 }
+                Log.d("visibility", "show");
 
+            }else{
+                BikeTrip bt = trip.getBikeTrip();
+                vh.depart_time.setText(bt.getDepartureTime().toString().substring(11, 16));
+                vh.arriv_time.setText(bt.getArrivalTime().toString().substring(11, 16));
+                vh.arriv_address.setText(bt.getTo().getAddress());
+                vh.from.setText(bt.getTransferStation().getName());
+                vh.mode.setText("By Callabike " + bt.getAvailability() + " bikes available at " + bt.getTransferStation().getName()
+                                +" Duration " + (int) bt.getDuration() + " min Distance " + (int) bt.getDistance() + " m");
+                Log.d("visibility", "gone");
             }
-        return convertView;
 
+        }
+        return convertView;
     }
 }
